d51b6cb1ce04ce89ff215eb2ee4ea2515e7730fd
T U <tylerletendre98@gmail.com>
2022-02-01 09:51:32 -0500

    added in routes to add members to a club as well as add messages to the club

--- Backend/routes/clubs.js
+++ Backend/routes/clubs.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Club = require('../models/club');
 const User = require('../models/user');
+const Message = require('../models/message')
 const router = express.Router();
 
 router.post('/newClub/:ownerId', async(req,res)=>{
@@ -32,6 +33,35 @@ router.get('getClub/:clubId', async (req,res)=>{
         return res.send(club);
     }catch(ex){
         return res.status(500).send(`Internal Server Error ${ex}.`)
+    }
+})
+
+router.post('/newMember/:clubId/:memberId', async(req,res)=>{
+    try{
+        const club = await Club.findById(req.params.clubId)
+        const newMember = await User.findById(req.params.memberId)
+        club.members.push(newMember)
+        club.save()
+        return res.send(club)
+    }catch(ex){
+        return res.status(500).send(`Internal Server Error ${ex}.`)
+    }
+})
+
+router.post('/newMessage/:clubId/:senderId', async (req,res)=>{
+    try{
+        const club = await Club.findById(req.params.clubId);
+        const sender = await User.findById(req.params.senderId)
+        const newMessage = new Message({
+            sender : sender.username,
+            text : req.body.text
+        })
+        newMessage.save()
+        club.messages.push(newMessage)
+        club.save()
+        return res.send(club)
+    }catch(ex){
+
     }
 })
 
