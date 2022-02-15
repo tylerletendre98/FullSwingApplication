4ef6e1f2b44d0dd173e1612e61ece756556a2d82
T U <tylerletendre98@gmail.com>
2022-02-15 13:03:55 -0500

    adding some of the components like header

--- FrontEnd/fullswingapplications/src/App.js
+++ FrontEnd/fullswingapplications/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import LandingPage from './Pages/landingPage/LandingPage';
+import ProfilePage from './Pages/profilePage/ProfilePage';
+import Header from './components/header/Header'
+import {useState} from 'react'
 
 function App() {
+
+  const [currentLoggedInUser, setCurrentLoggedInUser] = useState()
+
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
+        <Route path='/profilePage' element={<ProfilePage/>}/>
       </Routes>
     </div>
   );
---
+++ FrontEnd/fullswingapplications/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <div className='header-logo'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
