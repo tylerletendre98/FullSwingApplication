7327f4ddfe1f6e2330f25605b89dbf8750ab8985
T U <tylerletendre98@gmail.com>
2022-02-17 21:08:02 -0500

    adding in the login page and adding style to the header component

--- FrontEnd/fullswingapplications/src/App.js
+++ FrontEnd/fullswingapplications/src/App.js
@@ -1,21 +1,26 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
-import LandingPage from './Pages/landingPage/LandingPage';
-import ProfilePage from './Pages/profilePage/ProfilePage';
-import Header from './components/header/Header'
-import {useState} from 'react'
+import LandingPage from "./Pages/landingPage/LandingPage";
+import ProfilePage from "./Pages/profilePage/ProfilePage";
+import LoginPage from "./Pages/loginPage/LoginPage";
+import Header from "./components/header/Header";
+import { useState } from "react";
 
 function App() {
-
-  const [currentLoggedInUser, setCurrentLoggedInUser] = useState()
+  const [currentLoggedInUser, setCurrentLoggedInUser] = useState();
 
   return (
     <div className="App">
-      <Header/>
-      <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/profilePage' element={<ProfilePage/>}/>
-      </Routes>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/profilePage" element={<ProfilePage />} />
+          <Route path="/loginPage" element={<LoginPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- FrontEnd/fullswingapplications/src/components/header/Header.jsx
+++ FrontEnd/fullswingapplications/src/components/header/Header.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
 
 function Header() {
   return (
-    <div className='header-container'>
-        <div className='header-logo'>
-            
-        </div>
+    <div className="header-container">
+      <div className="header-name">
+        <h1>Full Swing Applications</h1>
+      </div>
+      <div className="header-buttons">
+        <Link to="/loginPage">
+          <button>Login</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
