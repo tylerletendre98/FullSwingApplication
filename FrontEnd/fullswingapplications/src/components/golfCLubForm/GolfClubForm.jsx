66075466397197d9c5bad9f142f7792dc0d3f802
T U <tylerletendre98@gmail.com>
2022-02-24 09:23:28 -0500

    golf club form

---
+++ FrontEnd/fullswingapplications/src/components/golfCLubForm/GolfClubForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function GolfClubForm() {
+  return (
+    <div>
+        <div>
+            <select name="" id="">
+                <option value="">Driver</option>
+                <option value="">3 Wood</option>
+                <option value="">5 Wood</option>
+                <option value="">2 Hybrid</option>
+                <option value="">3 Hybrid</option>
+                <option value="">4 Hybrid</option>
+                <option value="">5 Hybrid</option>
+                <option value="">Iron set</option>
+                <option value="">Approach Wedge</option>
+                <option value="">Gap Wedge</option>
+                <option value="">Pitching Wedge</option>
+                <option value="">Sand Wedge</option>
+                <option value="">48 Degre</option>
+                <option value="">4 Hybrid</option>
+                <option value="">4 Hybrid</option>
+            </select>
+        </div>
+    </div>
+  )
+}
+
+export default GolfClubForm
